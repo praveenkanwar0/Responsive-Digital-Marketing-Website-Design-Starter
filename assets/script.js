9092cdeab995765da0c8fa9c9e337ced5689ec64
Praveen Kanwar <praveen.kanwar0@gmail.com>
2023-01-01 11:41:26 +0545

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+/*================Sticky Navigation Bar===============*/
+
+
+
+/*================Navigation Bar Open/Close Toggle===============*/
+
+
+/*================Close Navigation Bar on Link Click===============*/
+
+
+
+/*================Testimonial Swiper initialization and configuration===============*/
+
+
+/*================Scroll Reveal Animations===============*/
+
+
+// scroll Home
+
+
+//scroll About
+
+//why choose us
+
+
+//services
+
+
+//testimonials
+
+
+//contact
+
+
+//footer
+
